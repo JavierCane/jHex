5c993817ecd26d004a54797ba4a52e5ded94c22f
Javier Ferrer <javier.mailserio@gmail.com>
2012-10-16 16:40:04 +0200

    Métodos de testing * Métodos de prueba para explicación GIT

--- Main.java
+++ Main.java
@@ -1,12 +1,40 @@
-public class Main
-{
+public class Main {
+
     /**
      * Método principal
      *
      * @param args
      */
-    public static void main ( String[] args )
+    public static void main( String[] args )
     {
-        System.out.println( "Hello, world!" );
+        decirHola( 3 );
+        decirCosas( "Cosas", 2 );
+    }
+
+    /**
+     * Saludar a mi madre tantas veces como le especifiquemos
+     *
+     * @param num_veces Número de veces a decir hola
+     */
+    private static void decirHola( Integer num_veces )
+    {
+        for ( Integer i = 0; i < num_veces; i++ )
+        {
+            System.out.println( i + ".- Hola mama!" );
+        }
+    }
+
+    /**
+     * Imprimir por pantalla lo que le indiquemos tantas veces como digamos
+     *
+     * @param num_veces Número de veces a decir el texto del 2º parámetro
+     * @param texto     Texto a imprimir por pantalla
+     */
+    private static void decirCosas( String texto, Integer num_veces )
+    {
+        for ( Integer i = 0; i < num_veces; i++ )
+        {
+            System.out.println( i + ".- " + texto );
+        }
     }
 }
