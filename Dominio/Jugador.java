de5ac0ce3fba20de47a79ce41b518cce71ef1e4f
GGSF1 <ggsf1@hotmail.com>
2012-10-21 22:20:06 +0200

    Nueva versión de Jugador y primera versión de Preferencias

--- Dominio/Jugador.java
+++ Dominio/Jugador.java
@@ -1,39 +1,56 @@
 package Dominio;
 
-// Aún hay que indentarlo bien y poner comentarios
-public class Jugador {
+public class Jugador
+{
 
-    private Integer fichas_puestas;
-    private Integer pistas_usadas;
-    private Float tiempo;
+	private Integer fichas_puestas;
+	private Integer pistas_usadas;
+	private Float tiempo;
 
-    public Jugador() {
-        fichas_puestas = new Integer(0);
-        pistas_usadas = new Integer(0);
-        tiempo = new Float(0.0);
-    }
+	public Jugador()
+	{
+		fichas_puestas = new Integer( 0 );
+		pistas_usadas = new Integer( 0 );
+		tiempo = new Float( 0.0 );
+	}
 
-    public Integer getFichasPuestas() {
-        return fichas_puestas;
-    }
+	public Integer getFichasPuestas()
+	{
+		return fichas_puestas;
+	}
 
-    public Integer getPistasUsadas() {
-        return pistas_usadas;
-    }
+	public Integer getPistasUsadas()
+	{
+		return pistas_usadas;
+	}
 
-    public Float getTiempo() {
-        return tiempo;
-    }
+	public Float getTiempo()
+	{
+		return tiempo;
+	}
 
-    public void setFichasPuestas(Integer f) {
-        this.fichas_puestas = f;
-    }
+	public void setFichasPuestas( Integer f )
+	{
+		this.fichas_puestas = f;
+	}
 
-    public void setPistasUsadas(Integer p) {
-        this.pistas_usadas = p;
-    }
+	public void setPistasUsadas( Integer p )
+	{
+		this.pistas_usadas = p;
+	}
 
-    public void setTiempo(Float t) {
-        this.tiempo = t;
-    }
+	public void setTiempo( Float t )
+	{
+		this.tiempo = t;
+	}
+
+	public void incrementarPistas()
+	{
+		pistas_usadas++;
+	}
+
+	public void incrementarFichas()
+	{
+		fichas_puestas++;
+	}
 }
