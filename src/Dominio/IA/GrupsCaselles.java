5bdb8facaebf3cde6b3a11a95f3782295a9c4703
Marc Junyent Martin <marc.juny@gmail.com>
2012-11-02 10:51:26 +0100

    clase auxiliar ia

---
+++ src/Dominio/IA/GrupsCaselles.java
@@ -0,0 +1,13 @@
+package Dominio.IA;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: marc
+ * Date: 31/10/12
+ * Time: 16:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class GrupsCaselles
+{
+
+}
