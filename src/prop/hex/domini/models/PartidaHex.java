d9547706fd6da3d480393cb9cfa884a5548b627c
GGSF1 <ggsf1@hotmail.com>
2012-11-10 23:05:18 +0100

    Modificado PartidaHex: - Añadidos todos los comentarios - Añadidos todos los setters - Añadido incrementar pistas

--- src/prop/hex/domini/models/PartidaHex.java
+++ src/prop/hex/domini/models/PartidaHex.java
@@ -54,6 +54,16 @@ public class PartidaHex extends Partida implements Serializable
 		temps_de_joc.put(jugador_b.getIdentificadorUnic(), 0);
 	}
 
+	/**
+	 * Mètode consultor de l’estat de la partida. Els paràmetres permeten aportar informació a sobre de l’últim moviment
+	 * d’interès realitzat (normalment l’últim realitzat correctament).
+	 *
+	 * @param fila    Fila del moviment d'interès
+	 * @param columna Columna del moviment d'interès
+	 * @return L'estat de la partida
+	 * @throws IndexOutOfBoundsException si (fila, columna) no és una coordenada dins dels límits del tauler on es
+	 *                                   desenvolupa la partida
+	 */
 	public EstatPartida comprovaEstatPartida( int fila, int columna ) throws IndexOutOfBoundsException
 	{
 		if ( !tauler.esCasellaValida( fila, columna ) )
@@ -154,13 +164,18 @@ public class PartidaHex extends Partida implements Serializable
 		}
 	}
 
+	/**
+	 * Consulta el nombre màxim de pistes que es poden utilitzar per jugador i partida.
+	 *
+	 * @return
+	 */
 	public static int getMaxNumPistes()
 	{
 		return max_num_pistes;
 	}
 
 	/**
-	 * Consulta el nombre de pistes usades d'un jugador
+	 * Consulta el nombre de pistes usades de l'usuari amb id_jugador.
 	 *
 	 * @param id_jugador Identificador únic del jugador.
 	 * @return El nombre de pistes utilitzades pel jugador.
@@ -180,7 +195,7 @@ public class PartidaHex extends Partida implements Serializable
 	}
 
 	/**
-	 * Consulta el temps de joc d'un jugador.
+	 * Consulta el temps de joc de l'usuari amb id_jugador.
 	 *
 	 * @param id_jugador Identificador únic del jugador.
 	 * @return El temps de joc que el jugador ha utilitzat a la partida.
@@ -200,12 +215,13 @@ public class PartidaHex extends Partida implements Serializable
 	}
 
 	/**
-	 * Modifica les pistes usades per l'usuari amb id_jugador
+	 * Modifica les pistes usades per l'usuari amb id_jugador.
 	 *
-	 * @param id_jugador Identificador únic del jugador
-	 * @param num_pistes Número de pistes usades pel jugador
+	 * @param id_jugador Identificador únic del jugador.
+	 * @param num_pistes Número de pistes usades pel jugador.
 	 * @return Cert si es modifiquen les pistes. Fals altrament.
-	 * @throws IllegalArgumentException
+	 * @throws IllegalArgumentException Si el jugador amb l'identificador únic passat com a paràmetre no juga la
+	 *                                  partida, o si el nonbre de pistes és negatiu.
 	 */
 	public boolean setPistesUsades( String id_jugador, int num_pistes ) throws IllegalArgumentException
 	{
@@ -213,10 +229,56 @@ public class PartidaHex extends Partida implements Serializable
 		{
 			throw new IllegalArgumentException( "El jugador no juga la partida." );
 		}
+		if ( num_pistes < 0 )
+		{
+			throw new IllegalArgumentException( "El nombre de pistes no pot ser negatiu." );
+		}
+		pistes_usades.put( id_jugador, num_pistes );
+		return true;
+	}
+
+	/**
+	 * Incrementa les pistes usades per l'usuari amb id_jugador.
+	 *
+	 * @param id_jugador Identificador únic del jugador.
+	 * @return Cert si es modifiquen les pistes. Fals altrament.
+	 * @throws IllegalArgumentException Si el jugador amb l'identificador únic passat com a paràmetre no juga la
+	 *                                  partida.
+	 */
+	public boolean incrementaPistesUsades( String id_jugador ) throws IllegalArgumentException
+	{
+		if ( !pistes_usades.containsKey( id_jugador ) )
+		{
+			throw new IllegalArgumentException( "El jugador no juga la partida." );
+		}
 		else
 		{
-			pistes_usades.put( id_jugador, num_pistes );
+			int nou_num_pistes = pistes_usades.get( id_jugador ) + 1;
+			pistes_usades.put( id_jugador, nou_num_pistes );
 			return true;
 		}
+	}
+
+	/**
+	 * Modifica les pistes usades per l'usuari amb id_jugador.
+	 *
+	 * @param id_jugador Identificador únic del jugador.
+	 * @param temps      Número de pistes usades pel jugador.
+	 * @return Cert si es modifiquen les pistes. Fals altrament.
+	 * @throws IllegalArgumentException Si el jugador amb l'identificador únic passat com a paràmetre no juga la
+	 *                                  partida, o si el temps de joc és negatiu..
+	 */
+	public boolean setTempsDeJoc( String id_jugador, int temps ) throws IllegalArgumentException
+	{
+		if ( !temps_de_joc.containsKey( id_jugador ) )
+		{
+			throw new IllegalArgumentException( "El jugador no juga la partida." );
+		}
+		if ( temps < 0 )
+		{
+			throw new IllegalArgumentException( "El temps de joc no pot ser negatiu." );
+		}
+		temps_de_joc.put( id_jugador, temps );
+		return true;
 	}
 }
