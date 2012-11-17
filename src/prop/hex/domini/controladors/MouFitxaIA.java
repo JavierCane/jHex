ddc468a3357d654b20eaf421919fc11719936171
Isaac <isaac@isb1009.es>
2012-11-17 13:31:36 +0100

    Añadido "interface" MouFitxaIA. *IMPORTANTE*: Debe cambiarse el nombre de InteligenciaArtificialHexFacil por InteligenciaArtificialHexFacilCtrl y añadir implements MouFitxaIA.

---
+++ src/prop/hex/domini/controladors/MouFitxaIA.java
@@ -0,0 +1,11 @@
+package prop.hex.domini.controladors;
+
+import prop.hex.domini.models.Casella;
+
+/**
+ * Interface per poder cridar als moviments de la intel·ligència artificial de manera genèrica.
+ */
+public interface MouFitxaIA
+{
+	public Casella mouFitxa();
+}
