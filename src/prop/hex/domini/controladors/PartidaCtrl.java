f2eca65a1072813c41df98f2ffd02bf88fa98cd5
Isaac <isaac@isb1009.es>
2012-11-19 13:08:23 +0100

    Añadidos llistaPartidesUsuari y esborraPartidesUsuari a PartidaCtrl.

--- src/prop/hex/domini/controladors/PartidaCtrl.java
+++ src/prop/hex/domini/controladors/PartidaCtrl.java
@@ -9,7 +9,7 @@ import prop.hex.gestors.PartidaHexGstr;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Date;
+import java.util.*;
 
 /**
  * Controlador de partida per al joc Hex.
@@ -41,7 +41,8 @@ public class PartidaCtrl
 	 * Tipus de fitxa de cada jugador.
 	 */
 	private static EstatCasella[] fitxes_jugadors = {
-			EstatCasella.JUGADOR_A, EstatCasella.JUGADOR_B
+			EstatCasella.JUGADOR_A,
+			EstatCasella.JUGADOR_B
 	};
 
 	/**
@@ -80,7 +81,8 @@ public class PartidaCtrl
 	 * @throws InstantiationError       Si hi ha problemes amb la instanciació de les intel·ligències artificials.
 	 * @throws IllegalArgumentException Si ja existeix una partida amb les dades donades creada en la mateixa data
 	 */
-	public static boolean inicialitzaPartida( int mida_tauler, UsuariHex jugador_a, UsuariHex jugador_b, String nom ) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException
+	public static boolean inicialitzaPartida( int mida_tauler, UsuariHex jugador_a, UsuariHex jugador_b, String nom )
+			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException
 	{
 		TaulerHex tauler = new TaulerHex( mida_tauler );
 		partida_actual = new PartidaHex( jugador_a, jugador_b, tauler, nom );
@@ -91,6 +93,63 @@ public class PartidaCtrl
 		}
 
 		inicialitzaEstructuresControl( jugador_a, jugador_b );
+
+		return true;
+	}
+
+	/**
+	 * Consulta les partides jugades per un usuari.
+	 *
+	 * @param id_usuari Identificador únic de l'usuari
+	 * @return Una llista amb les dades de les partides.
+	 */
+	public static HashMap<String, String>[] llistaPartidesUsuari( String id_usuari )
+	{
+		ArrayList<HashMap<String, String>> llista_partides = new ArrayList<HashMap<String, String>>();
+		Set<String> id_partides = gestor_partida.llistaPartidesUsuari( id_usuari );
+		for ( String id_partida : id_partides )
+		{
+			HashMap<String, String> info_partida = new HashMap<String, String>();
+			String[] camps = id_partida.split( "@" );
+
+			// Per poder obtenir les dades d'una data s'ha d'instanciar un objecte de tipus Calendar.
+			Calendar data = new GregorianCalendar();
+			data.setTime( new Date( Long.valueOf( camps[0] ) * 1000L ) );
+
+			// L'identificador s'inclou perquè el controlador de presentació pugui demanar la partida concreta.
+			info_partida.put( "identificador", id_partida );
+
+			// Es formaten la data i l'hora perquè no ho hagi de fer la vista.
+			info_partida.put( "data_hora", String.valueOf( data.get( Calendar.DAY_OF_MONTH ) ) + '/' +
+			                               String.valueOf( data.get( Calendar.MONTH ) ) + '/' +
+			                               String.valueOf( data.get( Calendar.YEAR ) ) + ' ' +
+			                               String.valueOf( data.get( Calendar.HOUR ) ) + ':' +
+			                               String.valueOf( data.get( Calendar.MINUTE ) ) + ':' +
+			                               String.valueOf( data.get( Calendar.SECOND ) ) );
+
+			info_partida.put( "nom", camps[2] );
+			info_partida.put( "jugador_a", camps[3] );
+			info_partida.put( "jugador_b", camps[4] );
+
+			llista_partides.add( info_partida );
+		}
+
+		return ( HashMap<String, String>[] ) llista_partides.toArray();
+	}
+
+	/**
+	 * Esborra les partides d'un usuari
+	 *
+	 * @param id_usuari Identificador únic de l'usuari de qui es volen esborrar les partides.
+	 * @return Cert si s'han esborrat. Fals altrament.
+	 */
+	public static boolean esborraPartidesUsuari( String id_usuari )
+	{
+		Set<String> id_partides = gestor_partida.llistaPartidesUsuari( id_usuari );
+		for ( String id_partida : id_partides )
+		{
+			gestor_partida.eliminaElement( id_partida );
+		}
 
 		return true;
 	}
@@ -112,8 +171,11 @@ public class PartidaCtrl
 	 * @throws InstantiationException   Si hi ha problemes quan s'intenta instanciar la partida o la intel·ligència
 	 *                                  artificial.
 	 * @throws IllegalArgumentException Si algun dels usuaris no juga a la partida.
+	 * @throws NullPointerException     Si
 	 */
-	public static boolean carregaPartida( String identificador_partida, UsuariHex jugador_a, UsuariHex jugador_b ) throws IOException, ClassNotFoundException, FileNotFoundException, IllegalAccessException, InstantiationException, IllegalArgumentException
+	public static boolean carregaPartida( String identificador_partida, UsuariHex jugador_a, UsuariHex jugador_b )
+			throws IOException, ClassNotFoundException, FileNotFoundException, IllegalAccessException,
+			       InstantiationException, IllegalArgumentException, NullPointerException
 	{
 		partida_actual = gestor_partida.carregaElement( identificador_partida );
 		gestor_partida.eliminaElement( identificador_partida );
@@ -152,16 +214,21 @@ public class PartidaCtrl
 	 *                                artificials.
 	 * @throws InstantiationError     Si hi ha problemes amb la instanciació de les intel·ligències artificials.
 	 */
-	private static void inicialitzaEstructuresControl( UsuariHex jugador_a, UsuariHex jugador_b ) throws ClassNotFoundException, IllegalAccessException, InstantiationException
+	private static void inicialitzaEstructuresControl( UsuariHex jugador_a, UsuariHex jugador_b )
+			throws ClassNotFoundException, IllegalAccessException, InstantiationException
 	{
 		usuaris_partida = new UsuariHex[2];
 		usuaris_partida[0] = jugador_a;
 		usuaris_partida[1] = jugador_b;
 
 		jugadors_ia = new MouFitxaIA[2];
-		jugadors_ia[0] = ( MouFitxaIA ) Class.forName( "prop.hex.domini.controladors." + jugador_a.getTipusJugador().getClasseCorresponent() ).newInstance();
+		jugadors_ia[0] = ( MouFitxaIA ) Class
+				.forName( "prop.hex.domini.controladors." + jugador_a.getTipusJugador().getClasseCorresponent() )
+				.newInstance();
 		jugadors_ia[0].setPartida( partida_actual );
-		jugadors_ia[1] = ( MouFitxaIA ) Class.forName( "prop.hex.domini.controladors." + jugador_b.getTipusJugador().getClasseCorresponent() ).newInstance();
+		jugadors_ia[1] = ( MouFitxaIA ) Class
+				.forName( "prop.hex.domini.controladors." + jugador_b.getTipusJugador().getClasseCorresponent() )
+				.newInstance();
 		jugadors_ia[1].setPartida( partida_actual );
 
 		darrera_fitxa = new Casella( 0, 0 );
@@ -197,8 +264,14 @@ public class PartidaCtrl
 		EstatPartida estat_actual = consultaEstatPartida();
 		if ( estat_actual != EstatPartida.NO_FINALITZADA )
 		{
-			UsuariCtrl.actualitzaEstadistiques( usuaris_partida[0], estat_actual == EstatPartida.GUANYA_JUGADOR_A, usuaris_partida[1].getTipusJugador(), partida_actual.getTempsDeJoc( usuaris_partida[0].getIdentificadorUnic() ), partida_actual.getTauler().getNumFitxesA() );
-			UsuariCtrl.actualitzaEstadistiques( usuaris_partida[1], estat_actual == EstatPartida.GUANYA_JUGADOR_B, usuaris_partida[0].getTipusJugador(), partida_actual.getTempsDeJoc( usuaris_partida[1].getIdentificadorUnic() ), partida_actual.getTauler().getNumFitxesA() );
+			UsuariCtrl.actualitzaEstadistiques( usuaris_partida[0], estat_actual == EstatPartida.GUANYA_JUGADOR_A,
+					usuaris_partida[1].getTipusJugador(),
+					partida_actual.getTempsDeJoc( usuaris_partida[0].getIdentificadorUnic() ),
+					partida_actual.getTauler().getNumFitxesA() );
+			UsuariCtrl.actualitzaEstadistiques( usuaris_partida[1], estat_actual == EstatPartida.GUANYA_JUGADOR_B,
+					usuaris_partida[0].getTipusJugador(),
+					partida_actual.getTempsDeJoc( usuaris_partida[1].getIdentificadorUnic() ),
+					partida_actual.getTauler().getNumFitxesA() );
 
 			Ranquing ranquing = Ranquing.getInstancia();
 			for ( UsuariHex usuari : usuaris_partida )
@@ -235,7 +308,8 @@ public class PartidaCtrl
 	 */
 	private static Casella movimentIA()
 	{
-		return jugadors_ia[partida_actual.getTornsJugats() % 2].mouFitxa( fitxes_jugadors[partida_actual.getTornsJugats() % 2] );
+		return jugadors_ia[partida_actual.getTornsJugats() % 2]
+				.mouFitxa( fitxes_jugadors[partida_actual.getTornsJugats() % 2] );
 	}
 
 	/**
@@ -278,17 +352,21 @@ public class PartidaCtrl
 			throw new UnsupportedOperationException( "La partida ja ha finalitzat" );
 		}
 
-		if ( !partida_actual.getTauler().esMovimentValid( fitxes_jugadors[partida_actual.getTornsJugats() % 2], fila, columna ) )
+		if ( !partida_actual.getTauler()
+				.esMovimentValid( fitxes_jugadors[partida_actual.getTornsJugats() % 2], fila, columna ) )
 		{
 			return false;
 		}
 		else
 		{
-			if ( partida_actual.getTauler().mouFitxa( fitxes_jugadors[partida_actual.getTornsJugats() % 2], fila, columna ) )
+			if ( partida_actual.getTauler()
+					.mouFitxa( fitxes_jugadors[partida_actual.getTornsJugats() % 2], fila, columna ) )
 			{
 				darrera_fitxa = new Casella( fila, columna );
 
-				partida_actual.incrementaTempsDeJoc( usuaris_partida[partida_actual.getTornsJugats() % 2].getIdentificadorUnic(), instant_actual - instant_darrer_moviment );
+				partida_actual.incrementaTempsDeJoc(
+						usuaris_partida[partida_actual.getTornsJugats() % 2].getIdentificadorUnic(),
+						instant_actual - instant_darrer_moviment );
 				partida_actual.incrementaTornsJugats( 1 );
 
 				// Per actualitzar l'estat de la partida en el controlador.
@@ -323,7 +401,8 @@ public class PartidaCtrl
 	 */
 	public static EstatPartida consultaEstatPartida()
 	{
-		EstatPartida estat_partida = partida_actual.comprovaEstatPartida( darrera_fitxa.getFila(), darrera_fitxa.getColumna() );
+		EstatPartida estat_partida =
+				partida_actual.comprovaEstatPartida( darrera_fitxa.getFila(), darrera_fitxa.getColumna() );
 
 		partida_actual.setFinalitzada( estat_partida != EstatPartida.NO_FINALITZADA );
 
@@ -376,5 +455,4 @@ public class PartidaCtrl
 	{
 		return partida_actual;
 	}
-
 }
