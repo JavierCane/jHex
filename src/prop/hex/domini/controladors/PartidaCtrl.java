6249ca341010a9210ef9f94c27e2a7c5ec97d079
Marc Junyent Martin <marc.juny@gmail.com>
2012-11-19 16:56:41 +0100

    Bug de PartidaCtrl solucionado con un parche, hay que investigarlo mejor.
    Trata de arrancarlo carlos, trata de arrancarlo por dios!

--- src/prop/hex/domini/controladors/PartidaCtrl.java
+++ src/prop/hex/domini/controladors/PartidaCtrl.java
@@ -401,7 +401,7 @@ public class PartidaCtrl
 	 */
 	public static EstatPartida consultaEstatPartida()
 	{
-		if(partida_actual == null )
+		if(darrera_fitxa == null )
 		{
 			return EstatPartida.NO_FINALITZADA;
 		}
