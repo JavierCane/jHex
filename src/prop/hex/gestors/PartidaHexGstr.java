4856ea691cc8f704412b6acc0848d3d26e1333d8
Isaac <isaac@isb1009.es>
2012-12-08 14:42:24 +0100

    [PartidaHexGstr] Cambio en llistaPartidesUsuari de HashSet a TreeSet para estar seguros de que las partidas están ordenadas por fecha.

--- src/prop/hex/gestors/PartidaHexGstr.java
+++ src/prop/hex/gestors/PartidaHexGstr.java
@@ -3,8 +3,8 @@ package prop.hex.gestors;
 import prop.hex.domini.models.PartidaHex;
 
 import java.io.File;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Classe per la gestió de la persistència de PartidaHex en disc, com s'estén de BaseGstr,
@@ -24,14 +24,15 @@ public final class PartidaHexGstr extends BaseGstr<PartidaHex>
 	/**
 	 * Llista els identificadors de les partides jugades per l'usuari amb identificador únic id_usuari.
 	 *
-	 * @return El conjunt d'identificadors de partides jugades per l'usuari amb identificador únic id_usuari.
+	 * @return El conjunt d'identificadors de partides jugades per l'usuari amb identificador únic id_usuari
+	 *         ordenades per data de creació.
 	 */
 	public Set<String> llistaPartidesUsuari( String id_usuari )
 	{
 		File carpeta = new File( carpeta_dades + '/' + subcarpeta_dades + '/' );
 		File[] llista_arxius = carpeta.listFiles();
 
-		Set<String> noms_elements = new HashSet<String>();
+		Set<String> noms_elements = new TreeSet<String>();
 		if ( llista_arxius != null )
 		{
 			for ( File arxiu : llista_arxius )
