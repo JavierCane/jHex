e33e06a6055f0ed13beeb090f6245af43291c7b9
Javier Ferrer <javier.mailserio@gmail.com>
2012-12-08 11:08:31 +0100

    Uso de la IA QueenBee para las sugerencias de jugadores (preferible tardar mucho menos que hacer sugerencias perfectas)

--- src/prop/hex/domini/models/enums/TipusJugadors.java
+++ src/prop/hex/domini/models/enums/TipusJugadors.java
@@ -19,8 +19,8 @@ import java.util.*;
  */
 public enum TipusJugadors
 {
-	CONVIDAT( 0, 0, 0, "IAHexSexSearch" ),
-	JUGADOR( 1, 5, 4, "IAHexSexSearch" ),
+	CONVIDAT( 0, 0, 0, "IAHexQueenBeeCtrl" ),
+	JUGADOR( 1, 5, 4, "IAHexQueenBeeCtrl" ),
 	IA_FACIL( 2, 10, 3, "IAHexFacilCtrl", "Jose Antonio Camacho (MiniMax, nivell 0)" ),
 	IA_MONTESCOUT( 3, 15, 2, "IAHexNegaMonteScout", "Bill Clinton (MonteScout, nivell 1)" ),
 	IA_QUEENBEE( 4, 15, 2, "IAHexQueenBeeCtrl", "Hillary Clinton (QueenBee, nivell 1.5)" ),
