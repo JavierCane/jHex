b87db92263d126c1ee063774a235dcd452b63361
Isaac <isaac@isb1009.es>
2012-12-15 18:29:07 +0100

    serialVersionUID de Usuari

--- src/prop/cluster/domini/models/Usuari.java
+++ src/prop/cluster/domini/models/Usuari.java
@@ -14,6 +14,11 @@ public class Usuari implements Serializable
 {
 
 	/**
+	 * ID de serialització
+	 */
+	protected static final long serialVersionUID = -6648226504410176256L;
+
+	/**
 	 * Nom (Identificador) de l'usuari.
 	 */
 	protected String nom;
