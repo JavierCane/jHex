427527383ed93301d425f310cc112dca6e699445
Isaac <isaac@isb1009.es>
2012-12-17 22:18:32 +0100

    Actualizados scripts de generación del jar

--- genera-jar-unix.sh
+++ genera-jar-unix.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
 
-rm -fr bin
-mkdir bin
+echo "Creant directori bin (si no existeix)"
+mkdir -p bin
+
+echo "Compilant fitxers font de src a bin"
 cd src
 javac -encoding UTF-8 -d ../bin $(find ./* | grep .java$)
+
+echo "Copiant les imatges de la interfície"
 cd ../bin
-cp -r ../img prop/img
-jar cfe JHex.jar prop.hex.presentacio.JHex ./*
-rm -fr prop
+cp -fr ../img prop/img
+
+echo "Creant el fitxer principal jHex.jar"
+jar cfe jHex.jar prop.hex.presentacio.JHex ./*
 cd ..
 
+echo "Creant els directoris jHex i jHex/doc (si no existeixen)"
+mkdir -p jHex/doc
+
+echo "Traslladant el fitxer bin/jHex.jar al directori jHex"
+mv -f bin/jHex.jar jHex/jHex.jar
+
+echo "Copiant la documentació al directori jHex/doc"
+cp -f doc/ajuda.pdf jHex/doc/ajuda.pdf
+
+echo "\nJa podeu executar el programa. Només cal que us traslladeu al"
+echo "directori jHex i executeu el fitxer jHex.jar amb la màquina virtual"
+echo "fent servir, per exemple, les comandes següents:"
+echo "\t cd ./jHex"
+echo "\t java -jar jHex.jar"
--- genera-jar-windows.bat
+++ genera-jar-windows.bat
@@ -1,10 +1,33 @@
+@echo off
+set TAB=
+	
+echo Creant directori bin (si no existeix)
 mkdir .\bin
+
+echo Compilant fitxers font de src a bin
 cd .\src
 javac -encoding UTF-8 -d ..\bin prop\hex\presentacio\*.java prop\hex\domini\controladors\*.java prop\hex\domini\controladors\IA\*.java
+
+echo Copiant les imatges de la interfície
 cd ..\bin
 xcopy /S /Y ..\img .\prop\img\
+
+echo Creant el fitxer principal jHex.jar
 jar cfe jHex.jar prop.hex.presentacio.JHex .\*
-mkdir ..\jHex
-xcopy /Y jHex.jar ..\jHex\
-del /F jHex.jar
 cd ..
+
+echo Creant els directoris jHex i jHex\doc (si no existeixen)
+mkdir .\jHex\doc
+
+echo Traslladant el fitxer bin\jHex.jar al directori jHex
+move /y .\bin\jHex.jar .\jHex\jHex.jar
+
+echo Copiant la documentació al directori jHex\doc
+xcopy /Y .\doc\ajuda.pdf .\jHex\doc\
+
+echo.
+echo Ja podeu executar el programa. Només cal que us traslladeu al
+echo directori jHex i executeu el fitxer jHex.jar amb la màquina virtual
+echo fent servir, per exemple, les comandes següents:
+echo %TAB% cd .\jHex
+echo %TAB% java -jar jHex.jar
