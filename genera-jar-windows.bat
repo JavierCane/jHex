c413ab1716d931f90d4d420db818705cce38f240
Isaac <isaac@isb1009.es>
2012-12-17 15:56:12 +0100

    Actualizado script para windows.

--- genera-jar-windows.bat
+++ genera-jar-windows.bat
@@ -1,10 +1,10 @@
-rmdir /s /q .\bin\
 mkdir .\bin
-cd .\src\
-for /r %%a in (*.java) do ( javac -encoding UTF-8 -d ..\bin "%%a" )
-cd ..\bin\
-jar cfe JHex.jar prop.hex.presentacio.JHex .\*
-cd ..\bin\
-xcopy /S ..\img\ .\prop\img\
-rmdir /s /q .\prop\
+cd .\src
+javac -encoding UTF-8 -d ..\bin prop\hex\presentacio\*.java prop\hex\domini\controladors\*.java prop\hex\domini\controladors\IA\*.java
+cd ..\bin
+xcopy /S /Y ..\img .\prop\img\
+jar cfe jHex.jar prop.hex.presentacio.JHex .\*
+mkdir ..\jHex
+xcopy /Y jHex.jar ..\jHex\
+del /F jHex.jar
 cd ..
